55bb9e0a39f3799bbf545595f6d329139d5bb6a6
ste9jpg <stejpg9@gmail.com>
2023-10-12 23:13:19 -0500

    General outline plus email validation
    I worked on creating an email validation

--- js/site.js
+++ js/site.js
@@ -6,6 +6,7 @@ const lname = document.getElementById('input-interested')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 
+//required fields that must be filled out 
 form.addEventListener('submit', (e) => {
   let messages = [];
 
@@ -20,3 +21,26 @@ form.addEventListener('submit', (e) => {
   if (email.value === '' || email.value == null) {
     messages.push('Email is required');
   }
+  
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email.value)) {
+    messages.push('Invalid email format');
+  }
+
+  if (phone.value === '' || phone.value == null) {
+    messages.push('Phone Number is required');
+  }
+  
+//email validation
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailPattern.test(email.value)) {
+    messages.push('Invalid email format');
+  }
+
+  if (phone.value === '' || phone.value == null) {
+    messages.push('Phone Number is required');
+  }
+
+//phone validation 
+
+//One radio button has to be checked
