d8905efcb3556656a7c96e3136938c585cdafe6b
ste9jpg <stejpg9@gmail.com>
2023-10-12 23:07:16 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -6,3 +6,17 @@ const lname = document.getElementById('input-interested')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 
+form.addEventListener('submit', (e) => {
+  let messages = [];
+
+  if (fname.value === '' || fname.value == null) {
+    messages.push('First Name is required');
+  }
+
+  if (lname.value === '' || lname.value == null) {
+    messages.push('Last Name is required');
+  }
+
+  if (email.value === '' || email.value == null) {
+    messages.push('Email is required');
+  }
