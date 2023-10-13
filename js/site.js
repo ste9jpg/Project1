3a5ca78414c43a1453c775d8c65a0626a7e06031
ste9jpg <stejpg9@gmail.com>
2023-10-12 23:04:04 -0500

    adding javascript file

---
+++ js/site.js
@@ -0,0 +1,8 @@
+const fname = document.getElementById('input-fname')
+const lname = document.getElementById('input-lname')
+const lname = document.getElementById('input-email')
+const lname = document.getElementById('input-phone')
+const lname = document.getElementById('input-interested')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
