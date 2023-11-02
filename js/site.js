698c714920fed6d184ad117851fc3953e6472072
ste9jpg <83249446+ste9jpg@users.noreply.github.com>
2023-11-02 17:31:55 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,33 +1,32 @@
-const fname = document.getElementById('input-fname')
-const lname = document.getElementById('input-lname')
-const lname = document.getElementById('input-email')
-const lname = document.getElementById('input-phone')
-const lname = document.getElementById('send')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+const fname = document.getElementById('input-fname');
+const lname = document.getElementById('input-lname');
+const lname = document.getElementById('input-email');
+const lname = document.getElementById('input-phone');
+const lname = document.getElementById('send');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
 
 //required fields that must be filled out 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', function(e) {
   let messages = [];
 
-  if (fname.value === '' || fname.value == null) {
+  if (fnameInput.value === '' || fnameInput.value == null) {
     messages.push('First Name is required');
   }
 
-  if (lname.value === '' || lname.value == null) {
+  if (lnameInput.value === '' || lnameInput.value == null) {
     messages.push('Last Name is required');
   }
 
-  if (email.value === '' || email.value == null) {
+  if (emailInput.value === '' || emailInput.value == null) {
     messages.push('Email is required');
+  } else {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(emailInput.value)) {
+      messages.push('Invalid email format');
+    }
   }
-  
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(email.value)) {
-    messages.push('Invalid email format');
-  }
-
-  if (phone.value === '' || phone.value == null) {
+  if (phoneInput.value === '' || phone.value == null) {
     messages.push('Phone Number is required');
   }
   
@@ -42,11 +41,14 @@ form.addEventListener('submit', (e) => {
   }
 
 //phone validation 
-  const phonePattern = /^\d{3}-\d{3}-\d{4}$/;
-  if (!phonePattern.test(phone.value)) {
-    messages.push('Invalid phone number format (e.g., 123-456-7890)');
+  if (phoneInput.value === '' || phoneInput.value === null) {
+    messages.push('Phone Number is required');
+  } else {
+    const phonePattern = /^\d{3}-\d{3}-\d{4}$/;
+    if (!phonePattern.test(phoneInput.value)) {
+      messages.push('Invalid phone number format (e.g., 123-456-7890)');
+    }
   }
-
 // button validation 
 button.addEventListener("click", function() {
     alert("Button Clicked!");
